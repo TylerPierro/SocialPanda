a27cd2256fa56d0775ca5ace44cedcfd1de21629
unknown <eric.fleming101@gmail.com>
2018-06-18 15:05:16 -0400

    started front end

---
+++ FrontEnd/src/App.tsx
@@ -0,0 +1,31 @@
+import * as React from 'react';
+
+import { NavComponent } from './components/nav.component';
+import './include/bootstrap';
+import './App.css';
+import { HashRouter, Switch, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { store } from './Store';
+import { SignInContainer } from './components/sign-in/sign-in.container';
+import { GroupsContainer } from './components/groups/groups.container';
+
+class App extends React.Component<any, any> {
+
+  public render() {
+    return (
+      <Provider store={store}>
+        <HashRouter>
+          <div>
+            <NavComponent />
+            <Switch>
+              <Route path="/sign-in" component={SignInContainer} />
+              <Route path="/groups" component={GroupsContainer} />
+            </Switch>
+          </div>
+        </HashRouter>
+      </Provider>
+    );
+  }
+}
+
+export default App;
