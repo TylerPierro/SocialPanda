7df84e5463a6aa04d1784cc67704eeab9726636b
unknown <eric.fleming101@gmail.com>
2018-06-18 15:17:05 -0400

    front end

--- FrontEnd/src/App.tsx
+++ FrontEnd/src/App.tsx
@@ -7,10 +7,7 @@ import { HashRouter, Switch, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './Store';
 import { SignInContainer } from './components/sign-in/sign-in.container';
-<<<<<<< HEAD
 import { GroupsContainer } from './components/groups/groups.container';
-=======
->>>>>>> f45ae6c83b4a9ec1e28ce541b3c57e8a69693e1a
 
 class App extends React.Component<any, any> {
 
@@ -22,10 +19,7 @@ class App extends React.Component<any, any> {
             <NavComponent />
             <Switch>
               <Route path="/sign-in" component={SignInContainer} />
-<<<<<<< HEAD
               <Route path="/groups" component={GroupsContainer} />
-=======
->>>>>>> f45ae6c83b4a9ec1e28ce541b3c57e8a69693e1a
             </Switch>
           </div>
         </HashRouter>
