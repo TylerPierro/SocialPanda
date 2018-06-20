9a80ef87385bcac7adca53bd6ca6775eb252110d
unknown <eric.fleming101@gmail.com>
2018-06-20 16:56:44 -0400

    updated

--- FrontEnd/src/reducers/index.ts
+++ FrontEnd/src/reducers/index.ts
@@ -2,8 +2,14 @@ import { combineReducers } from "redux";
 import { signInReducer } from "./sign-in.reducer";
 import { groupsReducer } from "./groups.reducer";
 import { Post } from "../model/Post";
-import { RegisterReducer } from "./register.reducer";
+// import { RegisterReducer } from "./register.reducer";
 import { CityTag } from "../model/CityTag";
+import { CognitoUser } from "amazon-cognito-identity-js";
+import { cognitoUserReducer } from "./cognito-user-reducer";
+
+export interface ICognitoUser { 
+  user: CognitoUser | null
+}
 
 export interface IGroups {
   citySearch: string;
@@ -18,19 +24,25 @@ export interface IRegister {
 }
 
 export interface ISignIn {
+  firstSignIn: {
+    isFirstSignIn: boolean,
+    password: string,
+    passwordConfirmation: string
+  },
   username: string,
   password: string,
   errorMessage: string
 }
 
 export interface IState {
+  cognitoUser: ICognitoUser,
   groups: IGroups,
   register: IRegister,
   signIn: ISignIn
 };
 
 export const state = combineReducers<IState>({
+  cognitoUser: cognitoUserReducer,
   groups: groupsReducer,
-  register: RegisterReducer,
   signIn: signInReducer
 });
