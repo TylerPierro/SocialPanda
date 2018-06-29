ac6edb4a9080ce250f8020cc627c02bc72e3ac29
unknown <eric.fleming101@gmail.com>
2018-06-28 21:07:38 -0400

    fixed profile

--- FrontEnd/src/components/environment.ts
+++ FrontEnd/src/components/environment.ts
@@ -2,12 +2,12 @@ const dev = {
   context: 'https://dwbbn4f58g.execute-api.us-east-2.amazonaws.com/dev',
   // Old Cognito
   // context: 'https://50re9cfzo7.execute-api.us-east-2.amazonaws.com/dev/',
-  gateway: 'https://dwbbn4f58g.execute-api.us-east-2.amazonaws.com/dev/'
+  gateway: 'https://dwbbn4f58g.execute-api.us-east-2.amazonaws.com/dev'
 }
 
 const prod = {
   context: 'http://ec2-52-15-153-131.us-east-2.compute.amazonaws.com:3000',
-  gateway: 'https://dwbbn4f58g.execute-api.us-east-2.amazonaws.com/dev/'
+  gateway: 'https://dwbbn4f58g.execute-api.us-east-2.amazonaws.com/dev'
 }
 
 
